698121435d829675b664fd5941bb046e772ef081
Joel Thiessen <dygerati@gmail.com>
2021-02-04 15:50:05 -0800

    Adding basic favorites endpoint

--- src/Features/Navigation/Favorite.php
+++ src/Features/Navigation/Favorite.php
@@ -56,46 +56,52 @@ class Favorite {
 	 * Add plugin to favorites
 	 *
 	 * @param string $plugin Identifier of plugin to add.
+	 * @throws \Exception Throws exception when item already exists.
 	 */
 	public static function add_plugin( $plugin ) {
 		$current_user_id = get_current_user_id();
 
 		if ( ! $current_user_id || ! $plugin ) {
-			return;
+			throw new \Exception( 'invalid_input' );
 		}
 
 		$all_favorites = self::get_all();
 
 		if ( in_array( $plugin, $all_favorites, true ) ) {
-			return;
+			throw new \Exception( 'already_exists' );
 		}
 
 		$all_favorites[] = $plugin;
 
 		self::set_meta_value( $current_user_id, $all_favorites );
+
+		return $all_favorites;
 	}
 
 	/**
 	 * Remove plugin from favorites
 	 *
 	 * @param string $plugin Identifier of plugin to remove.
+	 * @throws \Exception Throws exception when item does not exist.
 	 */
 	public static function remove_plugin( $plugin ) {
 		$current_user_id = get_current_user_id();
 
 		if ( ! $current_user_id || ! $plugin ) {
-			return;
+			throw new \Exception( 'invalid_input' );
 		}
 
 		$all_favorites = self::get_all();
 
 		if ( ! in_array( $plugin, $all_favorites, true ) ) {
-			return;
+			throw new \Exception( 'does_not_exist' );
 		}
 
-		$removed = array_diff( $all_favorites, [ $plugin ] );
+		$altered_favorites = array_values( array_diff( $all_favorites, [ $plugin ] ) );
 
-		self::set_meta_value( $current_user_id, array_values( $removed ) );
+		self::set_meta_value( $current_user_id, $altered_favorites );
+
+		return $altered_favorites;
 	}
 
 	/**
